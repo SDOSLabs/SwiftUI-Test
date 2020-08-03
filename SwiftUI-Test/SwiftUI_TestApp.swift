b41f082fa04a17d56af5a992baa5921d410858c8
Rafael Fernández <rafael.fernandez@sdos.es>
2020-08-03 13:24:15 +0200

    Initial Commit

---
+++ SwiftUI-Test/SwiftUI_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TestApp.swift
+//  SwiftUI-Test
+//
+//  Created by Rafael Fernandez Alvarez on 03/08/2020.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
